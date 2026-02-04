19575f19c6e6daaafac819b3d2931f81e7a72b14
Ashmit Gupta <59231064+NeuroCoder47@users.noreply.github.com>
2026-02-05 00:06:21 +0530

    Refactor data preprocessing to use Datatrove pipeline
    Replaced explicit shuffling with a pipeline-based approach using Datatrove for processing parquet files.

--- Pre Process Data/preprocess.py
+++ Pre Process Data/preprocess.py
@@ -1,65 +1,39 @@
-import os
-import numpy as np
-import random
-from pathlib import Path
-from tqdm import tqdm
-from tokenizers import Tokenizer
-from datasets import load_dataset
+from datatrove.executor.local import LocalPipelineExecutor
+from datatrove.pipeline.readers import ParquetReader
+from datatrove.pipeline.tokens import DocumentTokenizer
 
-tokenizer_path = r"C:\Users\Ashmit Gupta\Desktop\Coding\Pytorch\Transformer\GPT\Professional GPT\Tokenizer\data\tokenizer\tokenizer.json"
-output_path = Path(r"C:\Users\Ashmit Gupta\Desktop\Coding\Pytorch\Transformer\GPT\Professional GPT\Preprocess Data (Pre Train)")
-os.makedirs(output_path, exist_ok=True)
 
-def process_explicit_shuffle():
-    enc = Tokenizer.from_file(tokenizer_path)
-    bos_id = enc.token_to_id("<|bos|>")
+data_i =r"C:\Users\Ashmit Gupta\Desktop\Coding\Pytorch\Transformer\GPT\Professional GPT\Preprocess Data (Pre Train)\data_test\base_data"
+key = "text"
+opt = r"C:\Users\Ashmit Gupta\Desktop\Coding\Pytorch\Transformer\GPT\Professional GPT\Preprocess Data (Pre Train)\output_test"
+tok_pth = r"C:\Users\Ashmit Gupta\Desktop\Coding\Pytorch\Transformer\GPT\Professional GPT\Tokenizer\data\tokenizer\tokenizer.json"
+n_task = 2
+end = '<|eos|>'
+def main():
+    datatrove_reader = ParquetReader(
+        data_folder=data_i, 
+        text_key=key,
+        glob_pattern="*.parquet"  
+    )
     
-    print("Loading raw dataset...")
-    dataset = load_dataset("roneneldan/TinyStories", split='train')
-    
-   
-    print("Performing explicit index shuffle...")
-    indices = list(range(len(dataset)))
-    random.seed(42)
-    random.shuffle(indices) 
-    
-    split_idx = int(len(indices) * 0.99)
-    train_indices = indices[:split_idx]
-    val_indices = indices[split_idx:]
-    
-    splits = [('train', train_indices), ('val', val_indices)]
+    preprocess_executor = LocalPipelineExecutor(
+        pipeline=[
+            datatrove_reader,
+            DocumentTokenizer(
+                output_folder=opt,
+                tokenizer_name_or_path=tok_pth,
+                eos_token=end,
+                batch_size = 100,
+                shuffle_documents = False,
+                max_tokens_per_file=1e9,
+            ),
+        ],
+        tasks=n_task,
+        workers= 6,
+        start_method='spawn', 
+    )
+    preprocess_executor.run()
 
-    for split_name, split_indices in splits:
-        filename = os.path.join(output_path, f'{split_name}.bin')
-        print(f"\nWriting shuffled {split_name} to {filename}...")
-        
-     
-        with open(filename, 'wb') as f:
-            batch_size = 500 
-            token_buffer = []
-            
-            for i in tqdm(range(0, len(split_indices), batch_size)):
-               
-                batch_range = split_indices[i : i + batch_size]
-                
-                batch_examples = dataset.select(batch_range)
-                
-                for example in batch_examples:
-                    text_ids = enc.encode(example['text']).ids
-                    text_ids.insert(0, bos_id)
-                    token_buffer.extend(text_ids)
-                    
-                    if len(token_buffer) >= 100000:
-                        np_array = np.array(token_buffer, dtype=np.uint16)
-                        f.write(np_array.tobytes())
-                        token_buffer = [] 
-                
-            if token_buffer:
-                np_array = np.array(token_buffer, dtype=np.uint16)
-                f.write(np_array.tobytes())
 
-    print("\nProcessing Complete!")
-    print(f"Files saved in: {output_path}")
-
-if __name__ == '__main__':
-    process_explicit_shuffle()
+if __name__ == "__main__":
+    main()
