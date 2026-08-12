23d58ea0ec601ad491629fd70b2f6f37874f15b1
Ashmit Gupta <59231064+NeuroCoder47@users.noreply.github.com>
2026-08-12 16:06:27 +0530

    Add files via upload

--- Tokenizer/common.py
+++ Tokenizer/common.py
@@ -1,7 +1,6 @@
 def get_base_dir():
     # Hardcoded path to your D drive or specific folder
-    nanochat_dir = r"C:\Users\Ashmit Gupta\Desktop\Coding\Pytorch\Transformer\GPT\Professional GPT\Preprocess Data (Pre Train)\data_train\base_data" 
+    nanochat_dir = r"C:\Users\Ashmit Gupta\Desktop\Coding\Pytorch\Transformer\GPT\Professional GPT\Preprocess Data (Pre Train)\data_train" 
     save_path = r"C:\Users\Ashmit Gupta\Desktop\Coding\Pytorch\Transformer\GPT\Professional GPT\Tokenizer"
     return nanochat_dir, save_path
-
 
